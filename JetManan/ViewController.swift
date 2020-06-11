755493640f3a3992cc3bcf722b5a065af23341f4
Manan Shah <shah.pradipkumar@digital.datamatics.com>
2020-06-11 14:37:11 +0530

    Initial Commit

---
+++ JetManan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JetManan
+//
+//  Created by techjini on 11/06/20.
+//  Copyright © 2020 ms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
